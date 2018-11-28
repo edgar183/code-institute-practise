3a87c23dd8a6d4bea333d025d475dd0401a56da9
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2018-11-28 12:34:28 +0000

    initial commit

---
+++ spec/fizzbuzzSpec.js
@@ -0,0 +1,53 @@
+describe("FizzBuzz geme", function() {
+    describe("Fizz Buzz", function() {
+        it("should divide by 3 and 5", function() {
+            expect(fizzBuzz(15)).toBe("FizzBuzz")
+        });
+        it("should divide by 3", function() {
+            expect(fizzBuzz(9)).toBe("Fizz")
+        });
+        it("should divide by 5", function() {
+            expect(fizzBuzz(10)).toBe("Buzz")
+        });
+        it("should return 2 when called as fizzBuzz(2)", function() {
+            expect(fizzBuzz(2)).toBe(2)
+        });
+    });
+});
+/*
+describe("My fizzBuzz function", function() {
+    // A test suite begins with a call to the global Jasmine function describe with two parameters: a string and a function.
+    
+    beforeEach(function() {
+        fizzbuzz = new fizzBuzz();
+    });
+    
+    describe("Returns number, fizz, buzz or fizzbuzz", function() {
+        // Specs are defined by calling the global Jasmine function it
+        it("should exist", function() {
+            // Expectations are built with the function expect which takes a value, called the actual. 
+            // Each matcher implements a boolean comparison between the actual value and the expected value.
+            // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
+            expect(fizzBuzz).toBeDefined();
+        });
+        
+        it("should return Fizz when called as fizzBuzz(9)", function() {
+            var result = fizzBuzz(9)
+            expect(result).toBe("Fizz");
+        });
+                
+        it("should return Buzz when called as fizzBuzz(10)", function() {
+            var result = fizzBuzz(10)
+            expect(result).toBe("Buzz");
+        });
+        
+        it("should return FizzBuzz when called as fizzBuzz(15)", function() {
+            var result = fizzBuzz(15)
+            expect(result).toBe("FizzBuzz");
+        });
+        it("should return 2 when called as fizzBuzz(2)", function() {
+            var result = fizzBuzz(2)
+            expect(result).toBe(2);
+        });
+    });
+});*/
