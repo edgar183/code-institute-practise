0d23575f885d7b4ed21068f1591e1390b65fba38
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-02-07 20:49:30 +0000

    initila commit

---
+++ python.py
@@ -0,0 +1,13 @@
+"""
+from flask import  Flask
+def print_message(message):
+    print(message)
+    
+print_message('my name is edgars')
+"""
+x = 3
+y = 2
+#error message for testing the condition 
+assert x<y, '{0} should be less than {1}'.format(x,y)
+
+print(x+y)
